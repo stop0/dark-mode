539e1170c63e4c9268d93e346f74edaefba720a1
MRSTOP0\Stop0 <dawodmohmmed@gmail.com>
2021-04-28 05:52:02 -0400

    fix v3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
-import useLocalStorage from "./hooks/useLocalStorage"
 import Charts from "./components/Charts";
 import Navbar from "./components/Navbar";
 import useDarkMode from "./hooks/useDarkMode"
